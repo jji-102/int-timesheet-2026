c8a678556c34c1ce3688122f3c2d4e4432bf4033
jji-102 <51739271+jji-102@users.noreply.github.com>
2026-03-18 11:26:53 +0700

    Initial commit - Timesheet React App3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/int-timesheet-2026/', // เพิ่มบรรทัดนี้เข้าไป (ต้องมี / หน้าและหลัง)
 })
